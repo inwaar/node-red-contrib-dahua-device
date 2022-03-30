fea9ef56bbfcde9ff37e2bf2d27c6f702df84f62
FrankZZ <FrankZZ@users.noreply.github.com>
2022-03-30 23:11:55 +0200

    feat: add dahua-device-night & day

---
+++ dahua-device/dahua-device-night.js
@@ -0,0 +1,37 @@
+var ipcamera = require('node-dahua-api');
+
+module.exports = function (RED) {
+    function DahuaDeviceNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        var device = RED.nodes.getNode(config.device);
+
+        node.status({});
+        node.status({fill: 'yellow', shape: 'dot', text: 'connecting...'});
+
+        var dahua = new ipcamera.dahua({
+            host: device.ipaddress,
+            port: device.port,
+            user: device.credentials.username,
+            pass: device.credentials.password,
+            log: false,
+            cameraAlarms: false,
+        });
+
+        dahua.on('error', function (error) {
+            nodeStatus(node, 'red', 'error: ' + error);
+        });
+
+        dahua.nightProfile();
+        nodeStatus(node, 'green', 'success');
+    }
+
+    function nodeStatus(node, color, text) {
+        var options = { hour12: false, month: 'short', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+        var d = new Date();
+        var ds = d.toLocaleDateString('en-US', options);
+        node.status({fill: color, shape: 'dot', text: text + ' at ' + ds});
+    }
+
+    RED.nodes.registerType('dahua-device-night', DahuaDeviceNode);
+};
